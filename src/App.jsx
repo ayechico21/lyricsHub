81dcfe633d7fddbbc5ca7c7fe4839857906090b1
Maneesh Cheema <ayechico21@gmail.com>
2023-09-26 17:25:01 +0530

    style: title and body

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import Lyrics from "./pages/Lyrics";
 function App() {
   return (
     <Wrapper>
-      <Header>LyricsHUB</Header>
+      <Header>
+        Lyrics<HighlightText>HUB</HighlightText>
+      </Header>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -44,6 +46,18 @@ const Wrapper = styled.div`
 `;
 const Header = styled.h1`
   font-size: 2.5rem;
+  color: #645caa;
 `;
-
+const HighlightText = styled.span`
+  padding: 10px;
+  margin-left: 5px;
+  border-radius: 10px;
+  color: white;
+  background: linear-gradient(
+    45deg,
+    hsla(246, 31%, 51%, 1) 0%,
+    hsla(246, 31%, 53%, 1) 77%,
+    hsla(0, 0%, 89%, 1) 99%
+  );
+`;
 export default App;
